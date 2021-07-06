dd83db729733ab4509df82d6c972d961750c20ed
dev1 <dev1@gmail.com>
2021-07-06 14:15:11 +0530

    six

--- src/main/java/com/ashokit/test/Hello.java
+++ src/main/java/com/ashokit/test/Hello.java
@@ -1,7 +1,9 @@
 package com.ashokit.test;
 
 public class Hello {
-
+public void m1() {
+	System.out.println("Hello.m1()::abcd");
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("....radhe radhe");
